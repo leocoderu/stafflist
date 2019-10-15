274c179902240506e086296d2abfedd857853194
TravelTrace <ant_leo@mail.ru>
2019-10-15 16:51:15 +0300

    First Commit Create Repository

---
+++ StaffList/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using StaffList.Models;
+
+namespace StaffList.Controllers
+{
+    public class HomeController : Controller
+    {
+        //string connectionString = @"Data Source = (localdb)\MSSQLLocalDB;Initial Catalog = RSXB; Integrated Security = True";
+        string connectionString = @"Data Source = (LocalDb)\MSSQLLocalDB;Initial Catalog = RSXB; Integrated Security = True;AttachDBFilename=|DataDirectory|\RSXB.mdf";
+
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            DataTable Emp = new DataTable();
+            using (SqlConnection sqlCon = new SqlConnection(connectionString)) {
+                sqlCon.Open();
+                SqlDataAdapter sqlData = new SqlDataAdapter(
+                    "SELECT s.Id, s.Name, s.BirthDate, d.Name, p.Name, s.Automobile, s.EnterDate, s.LeaveDate, s.Married, s.Comments, s.Gender FROM Staff s LEFT OUTER JOIN Department d ON s.Department = d.Id LEFT OUTER JOIN Position p ON s.Position = p.Id", sqlCon);
+                sqlData.Fill(Emp);
+                sqlCon.Close();
+            }
+            return View(Emp);
+        }
+
+        public ActionResult Create() {
+            return View(new Employees());
+        }
+
+        /*
+        [HttpPost] 
+        public ActionResult Create(Employees Emp) {
+            return View("Index");
+        }*/
+    }
+}
---
+++ StaffList/Models/Employees.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StaffList.Models {
+    [DisplayName("Новый сотрудник")]
+    public class Employees {
+        [HiddenInput(DisplayValue = false)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Необходимо ввести Имя сотрудника")]
+        [Display(Name="ФИО сотрудника")]
+        public string Name { get; set; }
+        
+        [Required(ErrorMessage = "Необходимо указать день рождения сотрудника")]
+        [Display(Name = "Дата рождения")]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+                
+        [Required]
+        [Display(Name = "Пол")]
+        public bool Gender { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Дата приема на работу")]
+        public DateTime EnterDate { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Дата увольнения с работы")]
+        public DateTime LeaveDate { get; set; }
+
+        [Required]
+        [Display(Name = "Наличие автомобиля")]
+        public bool Automobile { get; set; }
+
+        [Required]
+        [Display(Name = "Семейное положение")]
+        public bool Married { get; set; }
+
+        [Required]
+        [Display(Name = "Отдел")]
+        public Department Department { get; set; }
+
+        [Required]
+        [Display(Name = "Должность")]
+        public Position Position { get; set; }
+
+        [Display(Name = "Комментарии")]
+        [UIHint("MultilineText")]
+        public string Comments { get; set; }
+    }
+
+    public class Department {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Position {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
